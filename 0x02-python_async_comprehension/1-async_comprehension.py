3c26210e9653d13b345eb3dc29767977f25b7c04
TLC <tituslifecharlie@gmail.com>
2024-10-15 23:25:44 +0100

    done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the async_comprehension coroutine."""
 from typing import List
-from .async_generator import async_generator
+from 0-async_generator import async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers from async_generator using async for."""
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,7 @@
 """This module contains the measure_runtime coroutine."""
 import asyncio
 from typing import List
-from .async_comprehension import async_comprehension
+from 1-async_comprehension import async_comprehension
 
 async def measure_runtime() -> float:
     """Measure the runtime of executing async_comprehension 4 times in parallel."""
